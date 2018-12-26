6e37eda65bb855a6a5d12753cd26eb07bcc6da01
Alena <mouse.programmouse@gmail.com>
2018-12-26 18:29:44 +0000

    deleted texts from html; add language swither on head tag; deleted normilize;

--- js/main.js
+++ js/main.js
@@ -1,33 +1,36 @@
-let slideIndex = 1;
-let lang = getDefaultLang();//get language by default
+var slideIndex = 1;
+var lang = getDefaultLang();//get language by default
 
-//next slide
+// next slide
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-//show current slide
+// show current slide
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  const slides = $(".slide");
-  const dots = $(".dot");
+  var i;
+  var slides = $(".slide");
+  var dots = $(".dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+  //hide slides
   for (i = 0; i < slides.length; i++) {
       slides[i].className = slides[i].className.replace(" active", " bottom");
   }
+  //hide dots
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+  //show selected slide and dot
   slides[slideIndex-1].className = slides[slideIndex-1].className.replace(" bottom", " active");
   dots[slideIndex-1].className += " active";
 }
 
-//set cookie
+// set cookie
 function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
@@ -35,7 +38,7 @@ function setCookie(cname, cvalue, exdays) {
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-//return cookie
+// return cookie
 function getCookie(cname) {
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
@@ -55,21 +58,21 @@ function getCookie(cname) {
 // Initialize and add the map
 function initMap() {
   // The location of studio
-  const studio = {lat: 49.4460794, lng: 32.0461428};
-  const pin = 'img/map_pin.svg';
-  const popupContent = "<div class=\"container\"><h2><a name=\"contacts\">Наши контакты</a></h2><div class=\"info\"><img src=\"img/logo_black.svg\" alt=\"logo\"/><div class=\"adress container\"><span>адрес:</span><span>ул. Благовисна 144, г.Черкассы</span></div><div class=\"phone container\"><span><a class=\"phone\" href=\"tel:+380731332930\">+380 73 133-2930</a><br/>Татьяна</span></div></div></div>";
+  var studio = {lat: 49.442916, lng: 32.049886};
+  var pin = 'img/map_pin.svg';
+  var popupContent = "<div class=\"container\"><h2><a name=\"contacts\">Наши контакты</a></h2><div class=\"info\"><img src=\"img/logo_black.svg\" alt=\"logo\"/><div class=\"adress container\"><span>адрес:</span><span>ул. Благовисна 144, г.Черкассы</span></div><div class=\"phone container\"><span><a class=\"phone\" href=\"tel:+380731332930\">+380 73 133-2930</a><br/>Татьяна</span></div></div></div>";
 
-  const map = new google.maps.Map(document.getElementById('map'), {zoom: 16, center: studio});
+  var map = new google.maps.Map(document.getElementById('map'), {zoom: 16, center: studio});
 
   // The marker, positioned at studio
-  const marker = new google.maps.Marker({
+  var marker = new google.maps.Marker({
       position: studio,
       map: map,
       icon: pin
   });
 
   //add some usefull info
-  const infowindow = new google.maps.InfoWindow({
+  var infowindow = new google.maps.InfoWindow({
     content: popupContent
   });
   //add eventlistener to pin
@@ -81,28 +84,41 @@ function initMap() {
 
 //get language by default
 function getDefaultLang() {
-  //TODO check cookie
-  const lang = getCookie("lang")
+  //check cookie
+  var lang = getCookie("lang");
   return (lang === "")?"RU":lang;//RU is lang by default
 }
 
-//translate content from dict
-//dict - global// JSON with translated data
-//lang - selected language
+
+// translate content from dict
+// dict - global// JSON with translated data
+// lang - selected language
 function translateContent(lang, dict) {
     $('[translate-key]').each(function(){
         $(this).text( dict[ lang ][$(this).attr('translate-key')]);
     });
 }
 
-//document loaded
+
+// set language for website
+function setLanguage(lang, pageLangSelector, dict) {
+  // change lan of page
+  pageLangSelector.attr("lang",lang);
+  // change lang of content
+  translateContent(lang, dict);
+}
+
+// document loaded
 $(function() {
-  const languageSelector = $(".lang");
-  const howSelector = $(".process-description");
+  //set language
+  var pageLangSelector = $("head");
+  setLanguage(lang, pageLangSelector, dict);
+  var languageSelector = $(".lang");
+  var howSelector = $(".process-description");
   showSlides(slideIndex);
-  const slideShowSelector = $(".slider");
-  const callToAction = $("#call-button");
-  const toTop = $(".go-to-top");
+  var slideShowSelector = $(".slider");
+  var callToAction = $("#call-button");
+  var toTop = $(".go-to-top");
 
   howSelector.on("click","h3", function(event) {
     howSelector.find(".line").not(".hide").addClass("hide");
@@ -111,17 +127,17 @@ $(function() {
     $(event.target).next(".description").removeClass("hide");
   });
 
-  //event listener for lang switcher
+  // event listener for lang switcher
   languageSelector.on("click","a", function (event) {
     event.preventDefault();
     if (event.target.innerHTML !== lang) {
-      //change language
+      // change language
       languageSelector.find(".checked").removeClass("checked");
       $(event.target).addClass("checked");
       lang = event.target.innerHTML;
       //translate page
-      translateContent(lang, dict);
-      //set/change lang cookie (live for 30 days)
+      setLanguage(lang, pageLangSelector, dict);
+      // set/change lang cookie (live for 30 days)
       setCookie("lang",lang,30);
     }
   });
