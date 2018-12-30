3461c9e22d7da16ab4e24cc5ad0a76700e3a708d
Alena <mouse.programmouse@gmail.com>
2018-12-30 20:11:07 +0000

    Added minified js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
+/**
+ * This class uses for manipulating with cookie
+ */
+// Constructor function
+function Cookie(cname) {
+    // cookie name
+    this.name = cname;
+
+    /**
+     * Method that stores the name and value cookie
+     *
+     * cname - name of the cookie
+     * cvalue - value of that cookie
+     * exdays - the number of days until the cookie should expire
+     */
+    this.setCookie = function (cvalue, exdays) {
+        // uses a Date object for manipulating with date
+        var d = new Date();
+        // calculated time in milliseconds
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        // set cookie name, cookie value and expire days
+        document.cookie = this.name + "=" + cvalue + ";" + expires + ";path=/";
+    };
+
+    /**
+     * Method that return the value of cookie by its name
+     * "" return by default
+     *
+     * cname - name of the cookie
+     */
+    this.getCookie = function () {
+        var name = this.name + "=";
+        // decodes a URI component
+        var decodedCookie = decodeURIComponent(document.cookie);
+        // split a string into an array of substrings,
+        // ";" is used as the separator
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            // if the cookie is found
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        // return "" if cookie is not founded
+        return "";
+    };
+}
+
+//-----------------------------------------------------------------------------
+
+
 var slideIndex = 1;
 // creating a Cookie object
 var myCookie = new Cookie("lang");
